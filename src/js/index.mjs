4272735f2478394e8020d541ee7ca661d28b0312
e0402 <100483961+e0402@users.noreply.github.com>
2023-05-17 23:15:56 +0200

    Adds switch statement to js/index file

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,26 +2,37 @@ import * as listeners from "./handlers/index.mjs";
 
 const path = location.pathname;
 
-if (path === "/profile/login/") {
-  listeners.setFormLoginListener();
-} else if (path === "/profile/register/") {
-  listeners.setFormRegistrationListener();
-} else if (path === "/listing/create/") {
-  listeners.setCreateListingFormListener();
-} else if (path === "/listing/edit/") {
-  listeners.setUpdateListingListener();
-} else if (path === "/profile/edit/") {
-  listeners.setUpdateProfileListener();
-} else if (path === "/listings/") {
-  listeners.readListings();
-} else if (path === "/listing/") {
-  listeners.readListing();
-} else if (path === "/listing/bid/") {
-  listeners.readBidOnListing();
-  listeners.setUpdateProfileListener();
-  listeners.setBidOnListingFormListener();
-} else if (path === "/profile/") {
-  listeners.readProfile();
-} else if (path === "/home/index.html") {
-  listeners.readListingsHomePage();
+switch (path) {
+  case "/profile/login/":
+    listeners.setFormLoginListener();
+    break;
+  case "/profile/register/":
+    listeners.setFormRegistrationListener();
+    break;
+  case "/listing/create/":
+    listeners.setCreateListingFormListener();
+    break;
+  case "/listing/edit/":
+    listeners.setUpdateListingListener();
+    break;
+  case "/profile/edit/":
+    listeners.setUpdateProfileListener();
+    break;
+  case "/listings/":
+    listeners.readListings();
+    break;
+  case "/listing/":
+    listeners.readListing();
+    break;
+  case "/listing/bid/":
+    listeners.readBidOnListing();
+    listeners.setUpdateProfileListener();
+    listeners.setBidOnListingFormListener();
+    break;
+  case "/profile/":
+    listeners.readProfile();
+    break;
+  case "/home/index.html":
+    listeners.readListingsHomePage();
+    break;
 }
